86cefb1586a13f4d00556b98bd9c84ef9356ccb4
Alfie Tones <alfietones0673@outlook.com>
2025-08-05 02:19:53 +0100

    refactor: Initialize main files and current order of expenses functions

--- expense.py
+++ expense.py
@@ -1,6 +1,5 @@
 class Expense:
-    def __init__(self, name, amount, category, date) -> None:
+    def __init__(self, name, amount, category) -> None:
         self.name = name
         self.amount = amount
         self.category = category
-        self.date = date
---
+++ expense_tracker.py
@@ -0,0 +1,26 @@
+def main():
+    print("Welcome to the Expense Tracker!")
+    pass
+
+    #Get user input for expenses
+    get_expense_details()
+
+    #Write expenses to a file
+    write_expense_to_file()
+
+    #Read expenses from a file and summarize them
+    summarize_expenses()
+
+def get_expense_details():
+    print("Getting expense details...")
+
+def write_expense_to_file():
+    print("Writing expense to file...")
+
+# This function will read expenses from a file and summarize them
+def summarize_expenses():
+    print("Summarizing expenses...")
+
+
+if __name__ == "__main__":
+    main()
